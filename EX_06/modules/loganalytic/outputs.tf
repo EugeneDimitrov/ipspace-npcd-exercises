3f0f2048bfa463aa5e7ef2deeef2a7a080b4f4ba
Eugene Dimitrov <eugene.dimitrov@gmail.com>
2021-04-18 17:19:57 +0500

    Update EX06 solution

--- EX_06/modules/loganalytic/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "log_analytic_id" {
-  value = azurerm_log_analytics_workspace.tf_log_analytic.workspace_id
-}
-
-output "log_analytic_key" {
-  value = azurerm_log_analytics_workspace.tf_log_analytic.primary_shared_key
-}
